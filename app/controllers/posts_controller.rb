6cf544191e882b115227036c3383476925ab91e9
nanomanon56 <manon.l.jean@gmail.com>
2017-11-09 23:06:59 -0500

    finle touches

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  http_basic_authenticate_with name: "Manon", password: "1234", except: [:index, :show] 
+
+	def index
+       @posts =Post.all
+	end	
+
+	def show 
+		@post =Post.find(params[:id])
+	end	
+
+	def new
+       @post =Post.new
+	end	
+
+	def create
+        @post =Post.new(post_params)
+
+        if(@post.save)
+        redirect_to @post
+        else
+    	render 'new'  
+  end
+  def edit
+        @post =Post.find(params[:id])
+  end
+    
+  def update
+    @post =Post.find(params[:id])
+
+        if(@post.update(post_params))
+        redirect_to @post
+        else
+           render 'edit' 
+         end   
+  end  
+
+     def destory
+      @post =Post.find(params[:id])
+      @post.destroy
+      redirect_to post_path
+     end  
+ 
+
+     private
+      def post_params
+        params.require(:post).permit(:title,:body)
+      end
+  end
+end
